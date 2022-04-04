041fc52371e070d57779d61117725fa237901cfa
Spencer Warneke <warnespe001@gmail.com>
2022-04-04 00:41:59 -0700

    Initial commit with customizations.

--- config.h
+++ config.h
@@ -5,21 +5,46 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
+
+
+// Colors
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+
+static const char col_drac_bg[]     = "#282a36";
+static const char col_drac_cl[]     = "#44475a";
+static const char col_drac_fg[]     = "#f8f8f2";
+static const char col_drac_comm[]   = "#6272a4";
+static const char col_drac_cyan[]   = "#8be9fd";
+static const char col_drac_green[]  = "#50fa7b";
+static const char col_drac_orange[] = "#ffb86c";
+static const char col_drac_pink[]   = "#ff79c6";
+static const char col_drac_purple[] = "#bd93f9";
+static const char col_drac_red[]    = "#ff5555";
+static const char col_drac_yellow[] = "#f1fa8c";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               tag_fg     tag_bg     border   */
+	[SchemeNorm] = { col_drac_fg, col_drac_bg, col_drac_cyan },
+	[SchemeSel]  = { col_drac_bg, col_drac_purple,  col_drac_purple  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = {
+    "dev",
+    "term",
+    "teams",
+    "ffx",
+    "*",
+    "*",
+    "*",
+    "disc",
+    "mus"
+};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -58,9 +83,12 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *roficmd[]  = { "rofi", "-show", "drun", "-show-icons", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_drac_bg, "-nf", col_drac_fg, "-sb", col_drac_purple, "-sf", col_drac_bg, NULL };
+static const char *termcmd[]  = { "kitty", NULL };
+static const char *roficmd[]  = { "rofi", "-no-lazy-grab", "-show", "drun", "-theme", "centered.rasi", NULL };
+static const char *killcmd[]  = { "pkill", "dwm", NULL };
+static const char *ffxcmd[]  = { "firefox", NULL };
+static const char *qtcmd[]  = { "/home/sanctity/QtCommercial/Tools/QtCreator/bin/qtcreator", "dwm", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -77,9 +105,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -98,10 +126,16 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { MODKEY|ShiftMask,             XK_x,      spawn,          {.v = killcmd } },
+    { MODKEY,                       XK_f,      spawn,          {.v = ffxcmd } },
+    { MODKEY,                       XK_c,      spawn,          {.v = qtcmd } },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+// Button 1 - Left Click
+// Button 2 - Middle Click
+// Button 3 - Right Click
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
